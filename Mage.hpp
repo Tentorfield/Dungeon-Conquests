8d26ac13de86fee5f3de99db1c6fa24631048b41
dliu117 <dliu117@ucr.edu>
2021-11-16 03:20:06 -0800

    update Mage.hpp Warrior.hpp

--- Mage.hpp
+++ Mage.hpp
@@ -13,7 +13,6 @@ class Mage{
 		double MagicalHit() = 0;
 		double SpecialHit() = 0;
 		void Bonus(int level, double AtkMult, double MagicMult, double HealthMult, double DefenseMult) = 0;
-
-
+};
 #endif
 		
--- Warrior.hpp
+++ Warrior.hpp
@@ -13,6 +13,5 @@ class Warrior{
 		double MagicalHit() = 0;
 		double SpecialHit() = 0;
 		void Bonus(int level, double AtkMult, double MagicMult, double HealthMult, double DefenseMult) = 0;
-
-
+};
 #endif
