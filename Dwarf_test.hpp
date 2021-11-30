52fcad2bc41a4d4995451248c70f90e854f00e6e
dliu117 <86322909+dliu117@users.noreply.github.com>
2021-11-30 11:54:55 -0800

    Update Dwarf_test.hpp

--- Dwarf_test.hpp
+++ Dwarf_test.hpp
@@ -3,28 +3,28 @@
 #include "gtest/gtest.h"
 #include "Dwarf.cpp"
 
-TEST(DwarfTest, getPhysicalDef(){
-	Dwarf* DwarfTest = new Dwarf();
-	DwarfTest->setPhysicalDef(20);
-	EXPECT_EQ(DwarfTest->getPhysicalDef();
-}
-
 TEST(DwarfTest, getMaxHealth_Test){
 	Dwarf* DwarfTest = new Dwarf();
 	DwarfTest->setMaxHealth(50);
 	EXPECT_EQ(DwarfTest->getMaxHealth(),50);
 }
 
-TEST(DwarfTest, getAtk_Test){
+TEST(DwarfTest, getAttack_Test){
 	Dwarf* DwarfTest = new Dwarf();
 	DwarfTest->setAttack(20);
-	EXPECT_EQ(DwarfTest->getAtk(),20);
+	EXPECT_EQ(DwarfTest->getAttack(),20);
 }
 
 TEST(DwarfTest, getMagicalDef_Test){
         Dwarf* DwarfTest = new Dwarf():
 	DwarfTest->setMagicalDef(20);
 	EXPECT_EQ(DwarfTest->getMagicalDef(),20);
+}
+
+TEST(DwarfTest, getPhysicalDef(){
+	Dwarf* DwarfTest = new Dwarf();
+	DwarfTest->setPhysicalDef(20);
+	EXPECT_EQ(DwarfTest->getPhysicalDef(),20);
 }
 
 #endif
