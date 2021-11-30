0baed9f8c25cb712df8eb0614b802bda295a3676
Tentorfield <58090168+Tentorfield@users.noreply.github.com>
2021-11-30 01:24:21 -0800

    Update Player.hpp

--- Player.hpp
+++ Player.hpp
@@ -1,89 +1,41 @@
-#ifndef PLAYER_HPP
-#define PLAYER_HPP
+#ifndef __PLAYER_HPP__
+#define __PLAYER_HPP__
 
 #include <iostream>
 #include <string>
 #include "stats/Stat.cpp"
-
+#include <cmath>
 using namespace std;
 
 class Player{
-	private:
+	protected:
 		string name;
-		Monster *Monster;
+		//Monster *Monster;
 		int level;
 		int expPoints;
-		
+		//int maxHealth;
+		//int currentHealth;
 		Stat* Stats;
 		// no need for a local copy of player stats
 		// when you want defense for example, just call Stats->getDefense();
-		
+
 		int freePoints;
 		int currentHealth;
-		
+
 	public:
-		Player(){ };
-		Player(string _name, double _health): name(_name), health(_health){}; 
-		void setExp(int _exp){
-			expPoints = _exp;
-		}
-		int getExp(){
-			return exp;
-		}
-		void setLevel(int _level){
-			level = _level;
-		}
-		int getLevel(){
-			return level;
-		}
-		
-		void setCurrentHealth(int val){
-			currentHealth = val;
-		}
+		Player();
+	//	Player(string _name, int _currenthealth, int _maxHealth int _level, int _expPoints, int _freePoints);
+		void setName(string _name);
+		string getName();
+		void setExp(int _exp);
+		int getExp();
+		void setLevel(int _level);
+		int getLevel();
+		void setCurrentHealth(int _currentHealth);
+		int getCurrentHealth();
+	//	void setHealth(int _health);
+		int getMaxHealth();
 
-		void getCurrentHealth(){
-			return this->currentHealth;
-		}
-	
-		void nextLevel(int _level){
-			level = _level;
-			if (Monster->isEliminate){
-				expPoints =  500 * pow(level, 2) - (500 * level);
-				level++;
-				freePoints += 10;
-				// freePoints are gained on level up. 
-				// The player can raise their stats by allocating points with 
-				// Stats->allocatePoints(int bodyAllocation, int mindAllocation, int spiritAllocation); 
-			}
-		}
-
-		string distributePoints() { 
-			int bodyAllocation, mindAllocation, spiritAllocation;
-			cout << "How many points would you like to invest in Body? ";
-			cin >> bodyAllocation;	
-			cout << "\nHow many points would you like to invest in Mind? ";
-			cin >> mindAllocation;
-			cout << "\nHow many points would you like to invest in Spirit? ";
-			cin >> spiritAllocation;
-			cout << endl;
-
-			if ( freePoints < (bodyAllocation + mindAllocation + spiritAllocation) ) {
-				return "insufficient free points";
-			}
-
-			Stats->allocatePoints(bodyAllocation, mindAllocation, spiritAllocation);
-			return "Stats successfully allocated. See new stats? (y/n) ";
-		}
-		
-		void displayPlayerStats() {
-			cout << "\n________________\n" << "Body: " << Stats->getBody() << "\nMind: " << Stats->getMind()<< "\nSpirit: " << Stats->getSpirit()
-			<< "\nPhysicalAttack Damage: " << Stats->getPhysicalAtk()<< "\nMagicalAttack Damage: " << Stats->getMagicAtk() 
-			<< "\nDefense: " << Stats->getDefense() << "\nMax Health: " << Stats->getMaxHealth() << "\n_______________" << endl;
-		}
-
-		//virtual void specialHit() = 0;
-		
-		// when you want attack, just call Stats->getPhysicalAtk or Stats->getMagicAtk
-
-};
-#endif 
+		void setPoints(int _freePoints);
+		int getPoints();
+		void nextLevel();
